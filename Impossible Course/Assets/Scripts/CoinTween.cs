88d5d921f8ce6a9d6eae42954b2820c0401f5dcd
Hasan Fuat Karahan <hasanfuathfk@gmail.com>
2025-02-04 20:08:39 +0300

    ui and game manager added

---
+++ Impossible Course/Assets/Scripts/CoinTween.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CoinTween : MonoBehaviour
+{
+    [SerializeField] private Transform uiCoinIcon;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<PlayerTagTwo>(out PlayerTagTwo boyTag))
+        {
+            RectTransform uiCoinRect = uiCoinIcon.GetComponent<RectTransform>();
+            transform.DOMove(uiCoinRect.position, 10f).SetEase(Ease.InOutQuad);
+        }
+    }
+}
